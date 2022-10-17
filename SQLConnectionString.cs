ccf06deb6c889ffa28a5fcd7d4918a5dc6908fe7
Code Kill <steve.rives@gmail.com>
2022-10-17 16:00:06 -0500

    Connection string capture class

---
+++ SQLConnectionString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+using STRATUS.CAD.Models.ConfigModels;
+
+namespace Replay
+{
+
+    /// <summary>
+    /// Inject into code through services to get me the DB string
+    /// </summary>
+    public class SQLConnectionString
+    {
+        public string ConnectionString { get; set; }
+
+        public SQLConnectionString(IOptionsMonitor<DatabaseConfig> optionsDatabase)
+        {
+            ConnectionString = optionsDatabase.CurrentValue.TelemetryDb;
+        }
+    }
+}
